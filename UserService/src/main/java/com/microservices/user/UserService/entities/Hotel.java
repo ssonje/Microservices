e0df5c2166dc2fcc57f557a25fd9b7cd578223bc
ssonje <ssonje99@gmail.com>
2022-12-25 10:37:26 +0530

    Setup Hotel properties

--- UserService/src/main/java/com/microservices/user/UserService/entities/Hotel.java
+++ UserService/src/main/java/com/microservices/user/UserService/entities/Hotel.java
@@ -1,4 +1,10 @@
 package com.microservices.user.UserService.entities;
 
 public class Hotel {
+
+    private Integer id;
+    private String name;
+    private String location;
+    private String about;
+
 }
