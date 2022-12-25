cff852716ae75a9131b8afed8fce0f512875af74
ssonje <ssonje99@gmail.com>
2022-12-25 15:46:49 +0530

    Migrate Rating service to the Feign Client

--- UserService/src/main/java/com/microservices/user/UserService/controllers/UserController.java
+++ UserService/src/main/java/com/microservices/user/UserService/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.microservices.user.UserService.entities.Hotel;
 import com.microservices.user.UserService.entities.Rating;
 import com.microservices.user.UserService.entities.User;
 import com.microservices.user.UserService.external.services.HotelService;
+import com.microservices.user.UserService.external.services.RatingService;
 import com.microservices.user.UserService.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,7 +27,8 @@ public class UserController {
     @Autowired
     private HotelService hotelService;
 
-    private static String RatingServiceBaseURL = "http://RATING-SERVICE/";
+    @Autowired
+    private RatingService ratingService;
 
     @PostMapping("/user/new")
     public ResponseEntity<?> createUser(@RequestBody User user) {
@@ -40,10 +41,7 @@ public class UserController {
         User user = userService.getUserFromID(userID);
 
         // Call the Rating service to set the data for user
-        Rating[] ratingsForUserID = restTemplate.getForObject(
-                UserController.RatingServiceBaseURL + "ratings/user/" + user.getId(),
-                Rating[].class);
-        List<Rating> ratings = Arrays.stream(ratingsForUserID).toList();
+        List<Rating> ratings = ratingService.getRatingsGivenByUserWithID(user.getId());
         List<Rating> ratingList = ratings.stream().map(rating -> {
             // Call the Hotel service to set the data for rating
             Hotel hotel = hotelService.getHotel(rating.getHotelID());
---
+++ UserService/src/main/java/com/microservices/user/UserService/external/services/RatingService.java
@@ -0,0 +1,16 @@
+package com.microservices.user.UserService.external.services;
+
+import com.microservices.user.UserService.entities.Rating;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "RATING-SERVICE")
+public interface RatingService {
+
+    @GetMapping("ratings/user/{userID}")
+    List<Rating> getRatingsGivenByUserWithID(@PathVariable("userID") Integer userID);
+
+}
